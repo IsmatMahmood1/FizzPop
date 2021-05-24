6b4425a9c22e207149bbc699ac6ec762c537b116
IsmMah <ismat.mahmood@softwire.com>
2021-05-24 23:41:22 +0100

    2nd commit

--- FizzPop.js
+++ FizzPop.js
@@ -1,1 +1,11 @@
-console.log ('Hello World')
+console.log ('this is FizzPop');
+for (let i=1;i<50;i++){
+    if (i%5 === 0 && i%3 ===0)
+        console.log('FizzPop')
+    else if (i%5 ===0)
+        console.log("Fizz");
+    else if (i%3 ===0)
+        console.log("Pop")
+    else
+        console.log(i);
+    }
